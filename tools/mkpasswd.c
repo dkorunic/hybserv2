e763b770aa79dd76cfce15eabc0ae9e107f8284d
wcampbel <wcampbel@234160a7-81ca-47ad-8fa5-976777e65532>
2001-08-28 12:34:04 +0000

    Synced up mkpasswd

--- tools/mkpasswd.c
+++ tools/mkpasswd.c
@@ -1,13 +1,13 @@
 /* simple password generator by Nelson Minar (minar@reed.edu)
- * copyright 1991, all rights reserved.
- * You can use this code as long as my name stays with it.
- *
- * md5 patch by Walter Campbell <wcampbel@botbay.net>
- * Modernization, getopt, etc for the Hybrid IRCD team
- *
- * $Id$
- */
-
+** copyright 1991, all rights reserved.
+** You can use this code as long as my name stays with it.
+**
+** md5 patch by Walter Campbell <wcampbel@botbay.net>
+** Modernization, getopt, etc for the Hybrid IRCD team
+** by Walter Campbell
+**
+** $Id$
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -18,20 +18,18 @@
 #define FLAG_DES     0x00000002
 #define FLAG_SALT    0x00000004
 #define FLAG_PASS    0x00000008
+#define FLAG_LENGTH  0x00000010
 
 extern char *getpass();
 extern char *crypt();
 
-
 char *make_des_salt();
-char *make_md5_salt();
+char *make_md5_salt(int);
 char *make_md5_salt_para(char *);
 void usage();
 static char saltChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
 
-int main(argc, argv)
-int argc;
-char *argv[];
+int main(int argc, char *argv[])
 {
   char *plaintext = NULL;
   extern char *optarg;
@@ -40,10 +38,11 @@ char *argv[];
   char *saltpara = NULL;
   char *salt;
   int flag = 0;
+  int length = 8;
 
   srandom(time(NULL));
 
-  while( (c=getopt(argc, argv, "mdhs:p:")) != -1)
+  while( (c=getopt(argc, argv, "mdh?l:s:p:")) != -1)
   {
     switch(c)
     {
@@ -52,6 +51,10 @@ char *argv[];
         break;
       case 'd':
         flag |= FLAG_DES;
+        break;
+      case 'l':
+        flag |= FLAG_LENGTH;
+        length = atoi(optarg);
         break;
       case 's':
         flag |= FLAG_SALT;
@@ -62,35 +65,49 @@ char *argv[];
         plaintext = optarg;
         break;
       case 'h':
+      case '?':
         usage();
         break;
       default:
+        printf("Invalid Option: -%c\n", c);
         break;
     }
   }
 
-  if (flag & FLAG_MD5) {
+  if (flag & FLAG_MD5)
+  {
     if (flag & FLAG_SALT)
       salt = make_md5_salt_para(saltpara);
     else
-      salt = make_md5_salt();
-  } else {
-    if (flag & FLAG_SALT) {
-      if ((strlen(saltpara) == 2)) {
+      salt = make_md5_salt(length);
+  }
+  else
+  {
+    if (flag & FLAG_SALT)
+    {
+      if ((strlen(saltpara) == 2))
+      {
         salt = saltpara;
-      } else {
+      }
+      else
+      {
         printf("Invalid salt, please enter 2 alphanumeric characters\n");
         exit(1);
       }
-    } else {
+    }
+    else
+    {
       salt = make_des_salt();
     }
   }
 
-  if (flag & FLAG_PASS) {
+  if (flag & FLAG_PASS)
+  {
     if (!plaintext)
       printf("Please enter a valid password\n");
-  } else {
+  }
+  else
+  {
     plaintext = getpass("plaintext: ");
   }
 
@@ -101,45 +118,55 @@ char *argv[];
 char *make_des_salt()
 {
   static char salt[3];
-  char* saltptr=salt;
   salt[0] = saltChars[random() % 64];
   salt[1] = saltChars[random() % 64];
   salt[2] = '\0';
-  return saltptr;
+  return salt;
 }
 
 char *make_md5_salt_para(char *saltpara)
 {
   static char salt[21];
-  char* saltptr=salt;
-  if (saltpara && (strlen(saltpara) <= 16)) {
+  if (saltpara && (strlen(saltpara) <= 16))
+  {
+    /* sprintf used because of portability requirements, the length
+    ** is checked above, so it should not be too much of a concern
+    */
     sprintf(salt, "$1$%s$", saltpara);
-    return saltptr;
+    return salt;
   }
   printf("Invalid Salt, please use up to 16 random alphanumeric characters\n");
   exit(1);
+
+  /* NOT REACHED */
+  return NULL;
 }
   
-char *make_md5_salt()
+char *make_md5_salt(int length)
 {
-  static char salt[13];
+  static char salt[21];
   int i;
-  char* saltptr=salt;
+  if (length > 16)
+  {
+    printf("MD5 salt length too long\n");
+    exit(0);
+  }
   salt[0] = '$';
   salt[1] = '1';
   salt[2] = '$';
-  for (i=3; i<=10; i++)
+  for (i=3; i<(length+3); i++)
     salt[i] = saltChars[random() % 64];
-  salt[11] = '$';
-  salt[12] = '\0';
-  return saltptr;
+  salt[length+3] = '$';
+  salt[length+4] = '\0';
+  return salt;
 }
 
 void usage()
 {
-  printf("mkpasswd [-m|-d] [-s salt] [-p plaintext]\n");
+  printf("mkpasswd [-m|-d] [-l saltlength] [-s salt] [-p plaintext]\n");
   printf("-m Generate an MD5 password\n");
   printf("-d Generate a DES password\n");
+  printf("-l Specify a length for a random MD5 salt\n");
   printf("-s Specify a salt, 2 alphanumeric characters for DES, up to 16 for MD5\n");
   printf("-p Specify a plaintext password to use\n");
   printf("Example: mkpasswd -m -s 3dr -p test\n");

44c6cf3e01590d50dea4aebec8ad2d239cc25dba
kreator <kreator@234160a7-81ca-47ad-8fa5-976777e65532>
2005-07-07 16:04:01 +0000

    - update hybrid_services.c from bane's repository

--- contrib/hybrid_services.c
+++ contrib/hybrid_services.c
@@ -1,54 +1,49 @@
 /*
- *  ircd-hybrid: an advanced Internet Relay Chat Daemon(ircd).
- *  hybrid_services.c: /nickserv, /chanserv, /memoserv, etc. commands, 
- *                     used for hybrid services (HybServ2) on 
- *                     CARNet IRC Network.
+ *   IRC - Internet Relay Chat, contrib/hybrid_services.c
+ *   hybrid_services.c: /nickserv, /chanserv, /memoserv, etc. commands, 
+ *                      used for hybrid services (HybServ2) on 
+ *                      idolNET IRC Network.
  *
- *  Copyright (C) 2002 by Dragan Dosen and the ircd-hybrid development team
- *  Based on m_services.c, originally from bahamut ircd.
+ *   Copyright (C) 2002, 2003, 2004, 2005 by Dragan 'bane' Dosen and the
+ *   Hybrid Development Team.
  *
- *  Contact info:
+ *   Based on m_services.c, originally from bahamut ircd.
  *
- *    E-mail : ddosen@ffzg.hr
- *    IRC    : (*) bane, idolNET IRC Network, irc.ffzg.hr, #twilight_zone
+ *   Contact info:
  *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are
- *  met:
+ *     E-mail : bane@idolnet.org
+ *     IRC    : (*) bane, idolNET, irc.idolnet.org, #twilight_zone
  *
- *  1.Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer. 
- *  2.Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution. 
- *  3.The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission. 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
  *
- *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *   $Id$
  */
 
 /* List of ircd includes from ../include/ */
 #include "stdinc.h"
+#include "tools.h"
 #include "handlers.h"
 #include "client.h"
 #include "common.h"     /* FALSE bleah */
+#include "hash.h"
+#include "hostmask.h"
 #include "ircd.h"
 #include "irc_string.h"
+#include "sprintf_irc.h"
 #include "numeric.h"
 #include "fdlist.h"
-#include "hash.h"
 #include "s_bsd.h"
 #include "s_conf.h"
 #include "s_log.h"
@@ -57,9 +52,6 @@
 #include "msg.h"
 #include "parse.h"
 #include "modules.h"
-#include "channel.h"
-#include "channel_mode.h"
-
 
 #define MAXLINE  512    /* don't change this */
 
@@ -88,39 +80,39 @@ static void m_identify(struct Client *client_p, struct Client *source_p,
  */
 struct Message global_msgtab = {
   "GLOBAL", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_global, m_ignore, m_ignore, m_global}
+  {m_unregistered, m_global, m_ignore, m_ignore, m_global, m_ignore}
 };
 struct Message nickserv_msgtab = {
   "NICKSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_nickserv, m_ignore, m_ignore, m_nickserv}
+  {m_unregistered, m_nickserv, m_ignore, m_ignore, m_nickserv, m_ignore}
 };
 struct Message chanserv_msgtab = {
   "CHANSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_chanserv, m_ignore, m_ignore, m_chanserv}
+  {m_unregistered, m_chanserv, m_ignore, m_ignore, m_chanserv, m_ignore}
 };
 struct Message memoserv_msgtab = {
   "MEMOSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_memoserv, m_ignore, m_ignore, m_memoserv}
+  {m_unregistered, m_memoserv, m_ignore, m_ignore, m_memoserv, m_ignore}
 };
 struct Message seenserv_msgtab = {
   "SEENSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_seenserv, m_ignore, m_ignore, m_seenserv}
+  {m_unregistered, m_seenserv, m_ignore, m_ignore, m_seenserv, m_ignore}
 };
 struct Message operserv_msgtab = {
   "OPERSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_operserv, m_ignore, m_ignore, m_operserv}
+  {m_unregistered, m_operserv, m_ignore, m_ignore, m_operserv, m_ignore}
 };
 struct Message statserv_msgtab = {
   "STATSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_statserv, m_ignore, m_ignore, m_statserv}
+  {m_unregistered, m_statserv, m_ignore, m_ignore, m_statserv, m_ignore}
 };
 struct Message helpserv_msgtab = {
   "HELPSERV", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_helpserv, m_ignore, m_ignore, m_helpserv}
+  {m_unregistered, m_helpserv, m_ignore, m_ignore, m_helpserv, m_ignore}
 };
 struct Message identify_msgtab = {
   "IDENTIFY", 0, 0, 0, 2, MFLG_SLOW, 0,
-  {m_unregistered, m_identify, m_ignore, m_ignore, m_identify}
+  {m_unregistered, m_identify, m_ignore, m_ignore, m_identify, m_ignore}
 };
 
 
@@ -129,15 +121,15 @@ struct Message identify_msgtab = {
  */
 struct Message ns_msgtab = {
   "NS", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_nickserv, m_ignore, m_ignore, m_nickserv}
+  {m_unregistered, m_nickserv, m_ignore, m_ignore, m_nickserv, m_ignore}
 };
 struct Message cs_msgtab = {
   "CS", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_chanserv, m_ignore, m_ignore, m_chanserv}
+  {m_unregistered, m_chanserv, m_ignore, m_ignore, m_chanserv, m_ignore}
 };
 struct Message ms_msgtab = {
   "MS", 0, 0, 1, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_memoserv, m_ignore, m_ignore, m_memoserv}
+  {m_unregistered, m_memoserv, m_ignore, m_ignore, m_memoserv, m_ignore}
 };
 
 
@@ -222,7 +214,7 @@ static void m_identify(struct Client *client_p, struct Client *source_p,
 
   if (parc == 3)
   {
-    acptr = find_person("ChanServ");
+    acptr = find_person(client_p, "ChanServ");
     if (!acptr || IsServer(acptr))
     {
       sendto_one(source_p,
@@ -235,10 +227,9 @@ static void m_identify(struct Client *client_p, struct Client *source_p,
       sendto_one(acptr, ":%s PRIVMSG ChanServ :IDENTIFY %s %s",
 	         parv[0], parv[1], parv[2]);
   }
-  else
-  if (parc == 2)
+  else if (parc == 2)
   {
-    acptr = find_person("NickServ");
+    acptr = find_person(client_p, "NickServ");
     if (!acptr || IsServer(acptr))
     {
       sendto_one(source_p,
@@ -271,7 +262,7 @@ static void m_identify(struct Client *client_p, struct Client *source_p,
  *
  *   parv[0]    = sender prefix
  *   servmsg    = message for services (using GetString() function).
- *                I think this is better. -ddosen
+ *                I think this is better. -bane
  */
 static void deliver_services_msg(char *service, char *command, struct
     Client *client_p, struct Client *source_p, int parc, char *parv[])
